1f5700d474c3c5d5ccb0ff26ab86c13c89908ecf
Antonio Masotti <masotti@bikeleasing.de>
2023-12-03 11:58:25 +0100

    fix: fix part2 - change strategy using regex for replacement

--- src/day1/Day01.kt
+++ src/day1/Day01.kt
@@ -17,34 +17,21 @@ fun main() {
 
 
     /**
-     * Task 2 - Numbers could be hidden as english names of the digits.
-     * Find the first and last occurrence of numbers, be them char digits or
-     * english names of the digits and add them together
+     * Task 2 - Numbers could be hidden as english names of the digits. Find
+     * the first and last occurrence of numbers, be them char digits or english
+     * names of the digits and add them together
      */
     fun part2(input: List<String>): Int {
         val digits = mutableListOf<Int>()
 
         input.map { line ->
-            val digitsInLineIndexed : MutableList<Pair<Int,Int>> = line
-                .mapIndexedNotNull {index, char ->
-                    char.digitToIntOrNull()?.let {
-                        digit -> Pair(digit, index)
-                    }
-                }.toMutableList()
-
-
-            digitsInLineIndexed.addHiddenNumber(line.findFirstHiddenNumber())
-            digitsInLineIndexed.addHiddenNumber(line.findLastHiddenNumber())
-
-            // Take the first digit, based on the indices (second element of each pair) and the last digit
-            // concatenate them and add them to the list of digits
-            val firstDigit = digitsInLineIndexed.minBy { it.second }.first
-            val lastDigit = digitsInLineIndexed.maxBy { it.second }.first
-
-            val winningDigit = "${firstDigit}${lastDigit}".toInt()
-            digits.add(winningDigit)
+            val calibrationDigit = line.findCalibrationNumber()
+            println("Input: $line --- Calibration digit: $calibrationDigit")
+            digits.add(calibrationDigit)
         }
 
+        check(digits.size == input.size) { "Not all lines were processed" }
+        println("Digits sum is: ${digits.sum()}")
         return digits.sum()
     }
 
@@ -58,5 +45,5 @@ fun main() {
     println(part2(input))
 
     check(part1(input) == 55208)
-    check(part2(input) == 54558)
+    check(part2(input) == 54578)
 }
--- src/day1/Utils.kt
+++ src/day1/Utils.kt
@@ -5,21 +5,13 @@ import java.security.MessageDigest
 import kotlin.io.path.Path
 import kotlin.io.path.readLines
 
-/**
- * Reads lines from the given input txt file.
- */
+/** Reads lines from the given input txt file. */
 fun readInput(name: String) = Path("src/day1/$name.txt").readLines()
 
-/**
- * Converts string to md5 hash.
- */
-fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray()))
-    .toString(16)
-    .padStart(32, '0')
+/** Converts string to md5 hash. */
+fun String.md5() = BigInteger(1, MessageDigest.getInstance("MD5").digest(toByteArray())).toString(16).padStart(32, '0')
 
-/**
- * Represents a map of strings to integers.
-*/
+/** Represents a map of strings to integers. */
 val stringIntMap = mapOf(
     "one" to 1,
     "two" to 2,
@@ -36,10 +28,8 @@ fun String.decodeNumber(): Int? {
     return stringIntMap[this.lowercase()]
 }
 
-fun String.findFirstHiddenNumber() : Pair<Int?,Int?> {
-    val matches = Regex("(zero|one|two|three|four|five|six|seven|eight|nine)")
-    .findAll(this)
-    .toList()
+fun String.findFirstHiddenNumber(): Pair<Int?, Int?> {
+    val matches = Regex("(zero|one|two|three|four|five|six|seven|eight|nine)").findAll(this).toList()
 
     val firstDigit = matches.firstOrNull()?.value?.decodeNumber()
     val index = matches.firstOrNull()?.range?.first
@@ -47,10 +37,8 @@ fun String.findFirstHiddenNumber() : Pair<Int?,Int?> {
     return Pair(firstDigit, index)
 }
 
-fun String.findLastHiddenNumber(): Pair<Int?,Int?> {
-    val matches = Regex("(zero|one|two|three|four|five|six|seven|eight|nine)")
-    .findAll(this)
-    .toList()
+fun String.findLastHiddenNumber(): Pair<Int?, Int?> {
+    val matches = Regex("(one|two|three|four|five|six|seven|eight|nine)").findAll(this).toList()
 
     val firstDigit = matches.lastOrNull()?.value?.decodeNumber()
     val index = matches.lastOrNull()?.range?.first
@@ -63,3 +51,76 @@ fun MutableList<Pair<Int, Int>>.addHiddenNumber(extractedNumber: Pair<Int?, Int?
         add(Pair(extractedNumber.first!!, extractedNumber.second!!))
     }
 }
+
+/**
+ * This was my second attempt (see above ext. functions to find hidden
+ * numbers). The regex approach is more clean, since it allows me to
+ * generalize the search in the main file
+ *
+ * But I have to trick it a little bit, searching for the first and last
+ * occurrence of the english number names as substrings. One error I needed
+ * quite a while to figure out was given by strings like "1sdasfiveightsd"
+ * Here the last number is 8 and not 5, but if I proceed in order I will
+ * replace five with 5 and then eight won't be found anymore (the "e" was
+ * eaten by "fivE").
+ */
+fun String.replaceStringInt(preferLast: Boolean = false): String {
+
+
+    val matches = mutableListOf<MatchResult>()
+
+    stringIntMap.keys.forEach { s ->
+        matches.addAll(
+            Regex(s, RegexOption.IGNORE_CASE).findAll(this).toList()
+        )
+    }
+
+    // As in part 1 - no numbers hidden with their english names
+    if (matches.isEmpty()) {
+        return this
+    }
+
+    var result = this
+
+    // To avoid the problem described above, sort either from the beginning or from the end
+    val sortedMatches = if (preferLast) {
+        matches.sortedByDescending { it.range.first }
+    } else {
+        matches.sortedBy { it.range.first }
+    }
+
+    sortedMatches.forEach { match ->
+        val number = match.value.decodeNumber()!!
+        result = result.replace(match.value, number.toString())
+    }
+
+    return result
+}
+
+
+fun String.findCalibrationNumber(): Int {
+    val digitIndexedSet = mutableSetOf<Pair<Int, Int>>()
+
+    // Replace from the beginning
+    digitIndexedSet.addAll(this.replaceStringInt().extractDigitsIndexed())
+
+    // Replace from the end
+    digitIndexedSet.addAll(this.replaceStringInt(true).extractDigitsIndexed())
+
+    // Use the indices to find the first real and last real digit
+    val firstDigit = digitIndexedSet.minBy { it.second }.first
+    val lastDigit = digitIndexedSet.maxBy { it.second }.first
+
+    val calibrationDigit = "${firstDigit}${lastDigit}".toInt()
+    return calibrationDigit
+}
+
+/**
+ * Go through the string, check each char if it's a digit, return the all
+ * the digits found with their index in the string.
+ */
+fun String.extractDigitsIndexed() = this.mapIndexedNotNull { index, char ->
+        char.digitToIntOrNull()?.let { digit ->
+            Pair(digit, index)
+        }
+    }.toMutableSet()

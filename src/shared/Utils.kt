5f3de7983c6f2d23008f9a0f36bb9b9b1af0a211
Antonio Masotti <masotti@bikeleasing.de>
2023-12-03 13:08:14 +0100

    feat: add utility fn

--- src/shared/Utils.kt
+++ src/shared/Utils.kt
@@ -7,3 +7,6 @@ import kotlin.io.path.readLines
  * Reads lines from the given input txt file.
  */
 fun readAdventInput(day: Int, name: String) = Path("src/day$day/$name.txt").readLines()
+
+
+fun printSectionSeparator() = println("".padEnd(50, '-'))

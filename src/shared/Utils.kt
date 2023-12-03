f933f798aa90885af6decb8a0e2f4f3bd437a5ac
Antonio Masotti <masotti@bikeleasing.de>
2023-12-03 12:13:42 +0100

    chore: prepare shared utils

---
+++ src/shared/Utils.kt
@@ -0,0 +1,9 @@
+package shared
+
+import kotlin.io.path.Path
+import kotlin.io.path.readLines
+
+/**
+ * Reads lines from the given input txt file.
+ */
+fun readAdventInput(day: Int, name: String) = Path("src/day$day/$name.txt").readLines()
